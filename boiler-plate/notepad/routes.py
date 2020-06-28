f74ec6f62c0abc77f6a58119b70d9659e80210cd
simran gulati <sgulati@sfu.ca>
2020-06-28 15:26:36 -0700

    added apis

--- boiler-plate/notepad/routes.py
+++ boiler-plate/notepad/routes.py
@@ -1,59 +1,89 @@
-from flask import render_template, url_for, flash, redirect, request
 from notepad import app, db
-from notepad.forms import NoteForm, UpdateForm
-from notepad.models import Note
+from flask import Flask, request, jsonify
+from flask_restful import abort
+
+# to do: 
+# figure out if we want to authenticate/authorize on each endpoint
+# organize project into multiple files
+# add error handling/validation once db has been added
+# add password reset API 
+# all non-admins are now called consumers
+
+class Login(Resource):
+    def post(self):
+        login_info = request.get_json()
+        # will change drastically when we implement login properly, this is just a dummy implementation
+        # return success/failure accordingly
+        return {"Message": "Successfully logged in as " + login_info['email']}, 200
+
+class SignUp(Resource):
+    def post(self):
+        sign_up_info = request.get_json()
+        # will change drastically when we implement login properly, this is just a dummy implementation
+        return {"Message": "Successfully signed up as " + sign_up_info['email']}, 201
+
+# Get all posts
+@app.route("/posts", methods=['GET'])
+def get_all_posts():
+    # dummy data for now, replace with db call  
+        client_post_1_info = {
+            'post_id': "128976",
+            'title': "Dummy Title 1",
+            'description': "Dummy Description 1",
+            'author_email': "Dummy_email1@gmail.com",
+            'date_posted': "10/06/2020",
+            'date_deadline': "10/07/2020",
+            'status': 'OPEN'
+        }
+        client_post_2_info = {
+            'post_id': "987654",
+            'title': "Dummy Title 2",
+            'description': "Dummy Description 2",
+            'author_email': "Dummy_email2@gmail.com",
+            'date_posted': "10/06/2020",
+            'date_deadline': "10/07/2020",
+            'status': 'IN_PROGRESS'
+        }
+        client_post_3_info = {
+            'post_id': "876543",
+            'title': "Dummy Title 3",
+            'description': "Dummy Description 3",
+            'author_email': "Dummy_email3@gmail.com",
+            'date_posted': "10/06/2020",
+            'date_deadline': "25/06/2020",
+            'status': 'CLOSED'
+        }
+        return jsonify(client_post_1_info, client_post_2_info, client_post_3_info), 200
+        
+# Create new post
+@app.route("/post/new", methods=['POST'])
+def create_new_post():
+    post_info = request.get_json()
+    # add db call in here to post information to the db
+    return {'Post saved! ': post_info}, 201 
 
 
-@app.route("/")
-@app.route("/home")
-def home():
-    notes = Note.query.all()
-    return render_template('home.html', notes=notes)
+# Get/Edit/Delete a single post
+@app.route("/posts/<int:post_id>", methods=['GET', 'PUT', 'DELETE'])
+def single_post(post_id):
+    if request.method == 'GET':
+        # replace with db call to get post info from db  
+        post_info = {
+            'post_id': post_id,
+            'title': "Dummy Title",
+            'description': "Dummy Description",
+            'author_email': "Dummy_email@gmail.com",
+            'date_posted': "10/06/2020",
+            'date_deadline': "10/07/2020",
+            'status': 'OPEN'
+        }
+        return {"Post retrieved ": post_info }, 200
+    elif request.method == 'PUT':
+        updated_post_info = request.get_json()
+        # add db call in here to update information in the db
+        return {'Post updated ': updated_post_info}, 200
+    elif request.method == 'DELETE':
+        post_to_delete = request.get_json()
+        # add db call to delete post from db
+        return {"Message": 'post with id ' + str(post_id) + ' has been deleted'}, 200
 
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
-
-@app.route("/note/new", methods=['GET', 'POST'])
-def new_note():
-    form = NoteForm()
-    if form.validate_on_submit():
-        note = Note(author=form.author.data, title=form.title.data, content=form.content.data)
-        db.session.add(note)
-        db.session.commit()
-        flash('Your note has been created!', 'success')
-        return redirect(url_for('home'))
-    return render_template('create_note.html', title='New Note', form=form, legend='New Note')
-
-
-@app.route("/note/<int:note_id>")
-def note(note_id):
-    note = Note.query.get_or_404(note_id)
-    return render_template('note.html', title=note.title, note=note)
-
-
-@app.route("/note/<int:note_id>/update", methods=['GET', 'POST'])
-def update_note(note_id):
-    note = Note.query.get_or_404(note_id)
-    form = UpdateForm()
-    if form.validate_on_submit():
-        note.title = form.title.data
-        note.content = form.content.data
-        db.session.commit()
-        flash('Your note has been updated!', 'success')
-        return redirect(url_for('note', note_id=note.id))
-    elif request.method == 'GET':
-        form.title.data = note.title
-        form.content.data = note.content
-    return render_template('create_note.html', title='Update Note', form=form, legend='Update Note')
-
-
-@app.route("/note/<int:note_id>/delete", methods=['POST'])
-def delete_note(note_id):
-    note = Note.query.get_or_404(note_id)
-    db.session.delete(note)
-    db.session.commit()
-    flash('Your note has been deleted!', 'success')
-    return redirect(url_for('home'))
