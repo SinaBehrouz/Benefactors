4d6a0658a68bbd11c5819bb9f3c2aa2808a4898a
Sina Ahmadian-Behrouz <ahmadian@sfu.ca>
2020-07-03 01:47:03 -0700

    added password reset changed to Docker env

--- web/benefactors/__init__.py
+++ web/benefactors/__init__.py
@@ -16,8 +16,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'fe568950@gmail.com'
-app.config['MAIL_PASSWORD'] = 'asdqwe123asdqwe123'
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 mail = Mail(app)
