2d2829b009d1d3540bbb363475357a1c4bb8a137
Sina Ahmadian-Behrouz <ahmadian@sfu.ca>
2020-08-07 18:54:47 -0700

    Search Func bug fixes + css fixes

--- web/benefactors/static/JS/home.js
+++ web/benefactors/static/JS/home.js
@@ -17,4 +17,16 @@ function initAutocomplete(){
     componentRestrictions:{'country': ['CA']},
     fields: ['place_id', 'geometry', 'name', 'address_components', 'geometry', 'icon']
   });
+  autocomplete.addListener('place_changed', onPlaceChanged);
+}
+function onPlaceChanged(){
+  update()
+}
+function update(){
+  var place = autocomplete.getPlace();
+  if (!place.geometry){ //user didnt click one of the offered places
+    document.getElementById('gSuggest').checked = false;
+  }else{
+    document.getElementById('gSuggest').checked = true;
+  }
 }
