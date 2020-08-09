0d614dec3d00667f1f820cd23446476df932832a
Martin Xaverius Jo <martinxaverius@gmail.com>
2020-08-08 23:09:51 -0700

    Add the seeding

--- web/app.py
+++ web/app.py
@@ -13,48 +13,48 @@ def create_db():
     db.create_all()
     db.session.commit()
 
-
+ 
 @cli.command("seed_db")
 def seed_db():
     # user: 1@1.com
     # password: 123123123
 
-    db.session.add(User(id =0, username="NULL", first_name="NULL", last_name="NULL", email="NULL", phone_number="NULL", postal_code="NULL", password="$2b$12$ppAg.mOnlIo15d0m7gPYr.1LZaUvuO29JVVBkv6bkQzQz6zK.f66y"))
-    db.session.add(User(username="sina123", first_name="Sina", last_name="Smith", email="1@1.com", phone_number="1111", postal_code="V3E3B5", password="$2b$12$ppAg.mOnlIo15d0m7gPYr.1LZaUvuO29JVVBkv6bkQzQz6zK.f66y"))
-    db.session.add(User(username="Arian123", first_name="Arian", last_name="Smith", email="2@2.com", phone_number="1111", postal_code="V5Z0G7", password="$2b$12$ppAg.mOnlIo15d0m7gPYr.1LZaUvuO29JVVBkv6bkQzQz6zK.f66y"))
-    db.session.add(User(username="Martin777", first_name="Martin", last_name="Jo", email="3@3.com", phone_number="12345", postal_code="V5Z0G7", password="$2b$12$ppAg.mOnlIo15d0m7gPYr.1LZaUvuO29JVVBkv6bkQzQz6zK.f66y"))
-    db.session.add(User(username="Simran122", first_name="Simran", last_name="Gulati", email="4@4.com", phone_number="54231", postal_code="V3Z4B2", password="$2b$12$ppAg.mOnlIo15d0m7gPYr.1LZaUvuO29JVVBkv6bkQzQz6zK.f66y"))
-    db.session.add(User(username="Hansen99", first_name="Hansen", last_name="William", email="5@5.com", phone_number="54321", postal_code="V3Z4B2", password="$2b$12$ppAg.mOnlIo15d0m7gPYr.1LZaUvuO29JVVBkv6bkQzQz6zK.f66y"))
+    db.session.add(User(id=0, username="NULL", first_name="NULL", last_name="NULL", email="NULL", phone_number="NULL", postal_code="NULL", password="$2b$12$ppAg.mOnlIo15d0m7gPYr.1LZaUvuO29JVVBkv6bkQzQz6zK.f66y"))
+    db.session.add(User(username="SamSmith", first_name="Sam", last_name="Smith", email="samsmith@gmail.com", phone_number="7782234554", postal_code="V3E3B5", password="$2b$12$ppAg.mOnlIo15d0m7gPYr.1LZaUvuO29JVVBkv6bkQzQz6zK.f66y", user_image="b06567b4c614462ab0ec5283d9d8b206.jpg"))
+    db.session.add(User(username="Taylah", first_name="Taylah", last_name="Rosa", email="trosa@yahoo.com", phone_number="6043454565", postal_code="V5Z0G7", password="$2b$12$ppAg.mOnlIo15d0m7gPYr.1LZaUvuO29JVVBkv6bkQzQz6zK.f66y", user_image="592d3d0ff7754784b50c7ed0f17dd499.jpg"))
+    db.session.add(User(username="Jezzie", first_name="Jess", last_name="Brown", email="jezzie@gmail.com", phone_number="6045557777", postal_code="V3H1L6", password="$2b$12$ppAg.mOnlIo15d0m7gPYr.1LZaUvuO29JVVBkv6bkQzQz6zK.f66y", user_image="ad2f902b324c4aca9284cc97baa73dfa.jpg"))
+    db.session.add(User(username="Jackie", first_name="Jacques", last_name="Tang", email="jjques@gmail.com", phone_number="7786562245", postal_code="V5K0A1", password="$2b$12$ppAg.mOnlIo15d0m7gPYr.1LZaUvuO29JVVBkv6bkQzQz6zK.f66y", user_image="744be000d8f94d5e8f44c0b0d83547ff.jpg"))
+    db.session.add(User(username="RL", first_name="Randall", last_name="Lowry", email="rrlowry@gmail.com", phone_number="6045001700", postal_code="V9Y8G9", password="$2b$12$ppAg.mOnlIo15d0m7gPYr.1LZaUvuO29JVVBkv6bkQzQz6zK.f66y", user_image="afb14fd47ff846efb8b73179275cd531.jpg"))
+    db.session.add(User(username="EricH", first_name="Eric", last_name="Singh", email="erichsingh@gmail.com", phone_number="7788787799", postal_code="V3T2E4", password="$2b$12$ppAg.mOnlIo15d0m7gPYr.1LZaUvuO29JVVBkv6bkQzQz6zK.f66y", user_image="02173210948342908c4e4087551747e7.jpg"))
+    db.session.add(User(username="Arsalan", first_name="Arsalan", last_name="Mackie", email="1@1.com", phone_number="7786637867", postal_code="V6B0M3", password="$2b$12$ppAg.mOnlIo15d0m7gPYr.1LZaUvuO29JVVBkv6bkQzQz6zK.f66y"))
 
-    db.session.add(Post(title="Clean Tesla", category="GROCERY", description="Really need help cleaning my Tesla. You can take it for a ride at the end.", user_id=1))
-    db.session.add(Post(title="Need a Book, Ideally Like the One in the Description", category="GROCERY", description="John Leak (c. 1892 – 1972) was an Australian recipient of the Victoria Cross, the highest award for gallantry in battle that could be awarded at that time to a member of the Australian armed forces. Leak enlisted in early 1915, and served with the 9th Battalion during the Gallipoli campaign. Along with his unit, he transferred to the Western Front, where he participated in the Battle of Pozières in July 1916. For his actions on 23 July during this battle he was awarded the Victoria Cross. He was seriously wounded at the Battle of Mouquet Farm in August. Suffering from the effects of his service, Leak was convicted of desertion in November 1917, but his sentence was ultimately suspended. In early March 1918 he was gassed, and saw no further combat before the Armistice of 11 November 1918. He returned to Australia and was discharged in 1919. After various jobs, Leak settled in South Australia in 1937 and died in 1972", user_id=1))
-    db.session.add(Post(title="Please Help Get My Groceires", category="GROCERY", description="Would really appreciate someone picking up some essential groceries. Eggs, bread, milk, that kind of thing.", user_id=2, volunteer=1))
-    db.session.add(Post(title="Need to Get My Medications", category="GROCERY", description="I'm a little scared to step out to get meds from my local pharmacy, would really appreciate if someone could pick them up for me", user_id=2, volunteer=1))
-    db.session.add(Post(title="SpaceX, Hire me", category="GROCERY", description="Elon, if you see this, hire me.", user_id=1))
-
-    db.session.commit()
-
-    db.session.add(PostComment(comment_desc="Thappis is a hello test comment from create DB", user_id=1, post_id=2))
-    db.session.add(PostComment(comment_desc="This is the second comment", user_id=1, post_id=2))
-
-    db.session.add(ChatChannel(user1_id=1, user2_id=3, last_updated = datetime.datetime(2020, 7, 19, 12, 5, 30)))
-    db.session.add(ChatChannel(user1_id=2, user2_id=3, last_updated = datetime.datetime(2020, 7, 25, 12, 5, 30)))
-    db.session.add(ChatChannel(user1_id=3, user2_id=4, last_updated = datetime.datetime(2020, 7, 21, 12, 5, 30)))
-    db.session.add(ChatChannel(user1_id=3, user2_id=5, last_updated = datetime.datetime(2020, 7, 20, 12, 5, 30)))
+    db.session.add(Post(title="A little help with Cleaning", category="CLEANING", description="I will not be home next Saturday at August 17th, and I would really glad if someone could help clean my house interiors and take care of my two cute dogs for few hours. The task will mostly be vacuuming and sweeping the the living room, play with the dogs, and feed the dogs. It will be a fun experience, I promise you. For anyone who is interested or have any more questions, please message me.", user_id=3))
+    db.session.add(Post(title="Mount TV and Assemble Furniture", category="LABOUR", description="Would really appreciate if someone can assist me in mounting up the TV and assemble a sofa-bed. I will have to prepare my open-house dinner and do not really have time to polish my living house because of the virus.", user_id=5))
+    db.session.add(Post(title="Bi-weekly Groceries Run", category="GROCERY", status="TAKEN", description="I would need someone to help me with my bi-weekly groceries to the nearest store. I would like to know where you live as well because I prefer someone who lives close to me. The groceries that I am buying are mostly food like canned food, meat, fresh vegetables, fruits, sometimes paper towel and toilet paper.  A little background on myself, I have grow old these last few days, and I am really scared of going out right now. I would appreciate if someone is kind enough and spend their time to help me with this simple task. Thank you to you all.", user_id=2, volunteer=6, date_posted=datetime.datetime(2020,7,31,10,0,0)))
+    db.session.add(Post(title="Need to Get My Medications A.S.A.P", category="MEDICATION", description="I'm in no condition to go out to my local pharmacy, and I would really appreciate if someone could buy them up for me. The medication I would like to buy are Tylenols, and maybe along with medical masks and hand sanitizers if they still exist on local store. I do really need them soon because I need to go work everyday using a bus, and I am risking myself everyday without mask and surgical mask.", user_id=2, volunteer=1))
+    db.session.add(Post(title="Need an extra hand or two hands to MOVING", category="MOVING", status="CLOSED", description="Hey it's the end of the month, and I am getting kicked out of my apartment. I would need someone that can drive a U-Haul truck to bring my furnitures, stuffs, and shoes. This is because I cannot drive and I have no money. Thank you for the help!", user_id=4, date_posted=datetime.datetime(2020,7,30,11,25,0)))
 
     db.session.commit()
 
-    db.session.add(ChatMessages(sender_id=1, message_content="Test message bruh", channel_id=1, message_time = datetime.datetime(2020, 7, 20, 12, 5, 30)))
-    db.session.add(ChatMessages(sender_id=3, message_content="Test message received bruh", channel_id=1, message_time = datetime.datetime(2020, 7, 20, 13, 6, 30)))
+    db.session.add(PostComment(comment_desc="Hi I am interested in helping, and I have sent you a message regarding the details of the task", user_id=6, post_id=3, date_posted=datetime.datetime(2020,8,1,11,6,0)))
+    db.session.add(PostComment(comment_desc="Thanks everyone, but I've been helped. Closing this post soon", user_id=2, post_id=3, date_posted=datetime.datetime(2020,8,2,15,20,55)))
+    db.session.add(PostComment(comment_desc="Hi I just saw this post and see this post is still open, do you still need help?", user_id=7, post_id=3, date_posted=datetime.datetime(2020,8,9,22,25,30)))
+    db.session.add(PostComment(comment_desc="Closing this post as I used an uber driver to move instead, I needed to move on July 31st!!", user_id=4, post_id=5, date_posted=datetime.datetime(2020,8,1,22,44,45)))
 
-    db.session.add(ChatMessages(sender_id=1, message_content="Test message bruh 2", channel_id=1, message_time = datetime.datetime(2020, 7, 20, 14, 5, 30)))
-    db.session.add(ChatMessages(sender_id=3, message_content="Test message received bruh 2", channel_id=1, message_time = datetime.datetime(2020, 7, 20, 15, 5, 30)))
-
-    db.session.add(ChatMessages(sender_id=1, message_content="Test message bruh 3", channel_id=1, message_time = datetime.datetime(2020, 7, 20, 16, 5, 30)))
-    db.session.add(ChatMessages(sender_id=3, message_content="Test message received bruh 3" , channel_id=1, message_time = datetime.datetime(2020, 7, 20, 17, 5, 30)))
+    db.session.add(ChatChannel(user1_id=2, user2_id=6, last_updated=datetime.datetime(2020,8,1,12,5,30)))
 
     db.session.commit()
 
+    db.session.add(ChatMessages(sender_id=6, message_content="Hi Taylah, My name is Eric. I just check your post on bi-weekly groceries run, and I am willing to help. Where do you live?", channel_id=1, message_time=datetime.datetime(2020,8,1,11,5,30)))
+    db.session.add(ChatMessages(sender_id=2, message_content="Hi Eric, I live at apartment across from Langara Station, the nearest grocery is T&T or Safeway around Oakridge area.", channel_id=1, message_time=datetime.datetime(2020,8,1,12,6,21)))
+    db.session.add(ChatMessages(sender_id=6, message_content="Oh perfect!! I would probably go more often to Safeway if that's okay with you, but I can go to T&T as well. When do you want to start and what do you want to deliver?", channel_id=1, message_time=datetime.datetime(2020,8,1,12,25,45)))
+    db.session.add(ChatMessages(sender_id=2, message_content="For this week, I would like to get eggs, gluten-free bread (Please make sure it's gluten free), and 2% Milk. I will pay you for sure through interac. Can I get your email, please?", channel_id=1, message_time=datetime.datetime(2020,8,1,13,26,44)))
+    db.session.add(ChatMessages(sender_id=6, message_content="Great! I will deliver it to you tomorrow morning from Safeway, if that's okay. ", channel_id=1, message_time=datetime.datetime(2020,8,2,15,27,22)))
+    db.session.add(ChatMessages(sender_id=2, message_content="Sounds great, see you tomorrow morning" , channel_id=1, message_time=datetime.datetime(2020,8,1,16,5,11)))
+    db.session.add(ChatMessages(sender_id=6, message_content="Hi Taylah, am outside your apartment with your groceries. My email is My email is haymond@gmail.com. It is around 24.3, you can check the receipt later.", channel_id=1, message_time=datetime.datetime(2020,8,2,9,33,33)))
+    db.session.add(ChatMessages(sender_id=2, message_content="Already? Thank you so much, Eric. I am heading down right now" , channel_id=1, message_time=datetime.datetime(2020,8,2,9,35,22)))
+
+    db.session.commit()
 
 if __name__ == "__main__":
     cli()
